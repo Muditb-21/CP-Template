d0d0feaa69682b30ef3cee5e486b1bb154f59cc3
Mudit <muditbhanwar@gmail.com>
2024-06-08 12:25:26 +0530

    Adding files

---
+++ main.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+// #include <algorithm>
+using namespace std;
+
+#define ll long long
+#define py cout << "YES" \
+                << "\n"
+#define pn cout << "NO" \
+                << "\n"
+#define f(i, a, b) for (ll i = a; i < b; i++)
+#define rf(i, b, a) for (ll i = b; i >= a; i--) // reverse For Loop
+#define pb push_back
+#define mp make_pair
+#define vl vector<ll>
+#define vpl vector<pair<ll, ll>>
+#define F first
+#define S second
+#define all(A) A.begin(), A.end()
+#define nl cout << endl;
+#define so(a) cout << a << " ";  // space seprated output
+#define no(a) cout << a << "\n"; // newline after printing
+#define i1(a) \
+    ll a;     \
+    cin >> a;
+#define i2(a, b) \
+    ll a, b;     \
+    cin >> a >> b;
+#define i3(a, b, c) \
+    ll a, b, c;     \
+    cin >> a >> b >> c;
+#define i4(a, b, c, d) \
+    ll a, b, c, d;     \
+    cin >> a >> b >> c >> d;
+#define show(a, n)             \
+    for (ll i = 0; i < n; i++) \
+    {                          \
+        cout << a[i] << " ";   \
+    }
+#define cina(a, n)             \
+    for (ll i = 0; i < n; i++) \
+    {                          \
+        cin >> a[i];           \
+    }
+#define cinaa(a, n, m)             \
+    for (ll i = 0; i < n; i++)     \
+    {                              \
+        for (ll j = 0; j < m; j++) \
+        {                          \
+            cin >> a[i][j];        \
+        }                          \
+    }
+#define showaa(a, n, m)             \
+    for (ll i = 0; i < n; i++)      \
+    {                               \
+        for (ll j = 0; j < m; j++)  \
+        {                           \
+            cout << a[i][j] << " "; \
+        }                           \
+        cout << "\n";               \
+    }
+#define zeroa(a, n)            \
+    for (ll i = 0; i < n; i++) \
+    {                          \
+        a[i] = 0;              \
+    }
+#define zeroaa(a, n, m)            \
+    for (ll i = 0; i < n; i++)     \
+    {                              \
+        for (ll j = 0; j < m; j++) \
+        {                          \
+            a[i][j] = 0;           \
+        }                          \
+    }
+#define mod 1000000007
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    //=============== CODE HERE ======================
+
+    return 0;
+}
